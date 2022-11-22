0bf019bc26fb2645b1b776bb3073ac8b4418bd4f
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-11-22 17:34:14 +0200

    Router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+// import store from './store'
+
+
+//tailwind
 import './style.css'
 import 'flowbite'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+// dark mode
+document.documentElement.classList.add('dark')
+
+const app = createApp(App)
+app.use(router)
+// app.use(store)
+app.mount('#app')
