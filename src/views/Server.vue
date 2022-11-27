785ddd4c58771e397d542bbaac90fab9948032e8
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-11-27 18:20:41 +0200

    Criteria UI enhanc.

--- src/views/Server.vue
+++ src/views/Server.vue
@@ -223,7 +223,11 @@
                         </div>
                         <div class="flex items-center space-x-4">
                             <button @click.prevent="addCriteria" :class="criteriaValid ? 'opacity: 100':'opacity: 50'" class="text-black bg-st-green hover:bg-st-green focus:ring-4 focus:outline-none focus:ring-st-green font-bold rounded-lg text-sm px-5 py-2.5 text-center bg-gradient-to-tr from-st-green to-st-sky dark:hover:bg-st-green dark:focus:ring-st-green">
-                                Create Criteria
+                                <svg v-if="btnLoader"  aria-hidden="true" role="status" class="inline mr-2 -mt-[0.5px] w-4 h-4 text-black animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="gray"/>
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                                </svg>
+                                {{ btnLoader ? 'Creating..' : 'Create Criteria' }}
                             </button>
                         </div>
                     </form>
@@ -254,7 +258,8 @@ export default {
                 description: 'gfgf',
             },
             guildCriteria: undefined,
-            loading: true
+            loading: true,
+            btnLoader: false
         }
     },
     components: {
@@ -276,21 +281,32 @@ export default {
             this.modalRole = undefined
         },
         async addCriteria() {
+            this.btnLoader = true
             // let criteria = {...this.criteria}
             // criteria.chainType = this.$CONSTANTS.ENUM.chains[this.criteria.chainType]
             // criteria.tokenStandard = this.$CONSTANTS.ENUM.standards[this.criteria.tokenStandard]
             console.log(this.criteria)
-            const resp = await axios.get('/api/criteria/add', {
+            await axios.get('/api/criteria/add', {
                 params: {
                     modId: this.$store.state.discordId,
                     guildId: this.serverData.id,
                     roleId: this.modalRole.id,
                     criteria: JSON.stringify(this.criteria)
                 }
-            }).finally(
-                this.loading = false
-            )
-            
+            })    
+            this.modalUp = false
+            this.loading = false
+            this.btnLoader = false
+            this.getGuildCriteria()
+        },
+        async getGuildCriteria() {
+            this.loading = true
+            this.guildCriteria = (await axios.get('/api/criteria/getGuildCriteria', {
+                params: {
+                    guildId: this.serverData.id
+                }
+            })).data.guildCriteria
+            this.loading = false
         }
     },
     computed: {
@@ -321,12 +337,7 @@ export default {
         //         id: this.$store.state.discordId
         //     }
         // })
-        this.guildCriteria = (await axios.get('/api/criteria/getGuildCriteria', {
-            params: {
-                guildId: this.serverData.id
-            }
-        })).data.guildCriteria
-        this.loading = false
+        this.getGuildCriteria()
     }
 }
 </script>
