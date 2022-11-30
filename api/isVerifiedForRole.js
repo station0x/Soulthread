a578a86934663a73930237091ec6fe42e8606217
elrakabawi <mselrakabawi@gmail.com>
2022-11-30 10:13:13 +0200

    isVerified endpoint

---
+++ api/isVerifiedForRole.js
@@ -0,0 +1,15 @@
+"use strict";
+// Import the dependency.
+const clientPromise = require('../api-utils/mongo-client');
+
+module.exports = async (req, res) => {
+    console.log('fired...')
+    const { userHash } = req.query
+    if(!userHash) throw new Error('User hash was not provided')
+    const client = await clientPromise;
+    const db = client.db()
+    const verifiedMembers = db.collection("verifiedMembers")
+    const userDoc = (await verifiedMembers.find({id: userHash}).limit(1).toArray())[0]
+    if(userDoc) res.status(200).json({ rolesPassed: userDoc.rolesPassed });
+    else throw new Error("User verification record does not exist")
+}
